1ecc1ce0f80a7039cb803b04fbbf597a8abcc002
Ole Konrad Ruus <145281506+oleruus@users.noreply.github.com>
2026-05-10 16:52:02 +0200

    Add files via upload

--- RL_controller_code/createAgent_pitch_angle_i.m
+++ RL_controller_code/createAgent_pitch_angle_i.m
@@ -1,20 +1,28 @@
 clear pitch_angle_i obsInfo actInfo initOpts
 
-obsInfo = rlNumericSpec([1 1],'LowerLimit',-inf,'UpperLimit',inf);
-obsInfo.Name = 'observation';
+obsInfo = rlNumericSpec([2 1], ...
+    'LowerLimit', [-inf; -inf], ...
+    'UpperLimit', [ inf;  inf]);
+obsInfo.Name        = 'observations';
+obsInfo.Description = 'pitch_err, pitch';
 
-actInfo = rlNumericSpec([1 1],'LowerLimit',-1,'UpperLimit',1);
+actInfo = rlNumericSpec([1 1], ...
+    'LowerLimit', -1, ...
+    'UpperLimit',  1);
 actInfo.Name = 'Ki_action';
 
 initOpts = rlAgentInitializationOptions;
-initOpts.NumHiddenUnit = 32;
+initOpts.NumHiddenUnit = 64;
 
-pitch_angle_i = rlDDPGAgent(obsInfo,actInfo,initOpts);
+pitch_angle_i = rlDDPGAgent(obsInfo, actInfo, initOpts);
 
-pitch_angle_i.AgentOptions.SampleTime = 0.02;
-pitch_angle_i.AgentOptions.DiscountFactor = 0.99;
-pitch_angle_i.AgentOptions.MiniBatchSize = 64;
-pitch_angle_i.AgentOptions.ExperienceBufferLength = 1e5;
-pitch_angle_i.AgentOptions.TargetSmoothFactor = 1e-3;
-pitch_angle_i.AgentOptions.NoiseOptions.Variance = 0.2;
-pitch_angle_i.AgentOptions.NoiseOptions.VarianceDecayRate = 1e-4;
+pitch_angle_i.AgentOptions.SampleTime                     = 0.02;
+pitch_angle_i.AgentOptions.DiscountFactor                 = 0.99;
+pitch_angle_i.AgentOptions.MiniBatchSize                  = 128;
+pitch_angle_i.AgentOptions.ExperienceBufferLength         = 1e6;
+pitch_angle_i.AgentOptions.TargetSmoothFactor             = 1e-3;
+pitch_angle_i.AgentOptions.NoiseOptions.Variance          = 0.3;
+pitch_angle_i.AgentOptions.NoiseOptions.VarianceDecayRate = 1e-5;
+
+disp('Pitch angle I agent created:')
+disp(pitch_angle_i)
